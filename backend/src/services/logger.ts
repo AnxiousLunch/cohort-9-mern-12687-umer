ab120e651b4b92193630fd64dee980ba30ef0500
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-01 19:20:21 +0500

    Basic init for pino logger

--- backend/src/services/logger.ts
+++ backend/src/services/logger.ts
@@ -1,1 +1,15 @@
 // Serves as a starting point for pino logger
+
+import pino from "pino";
+
+const logger = pino({
+  level: 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+export default logger
