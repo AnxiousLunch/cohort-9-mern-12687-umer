eb81e072530ef8309f2c099590077ddfe2771bd5
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-01 19:37:36 +0500

    Hook up logger with pino http and express app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express, {type Express, type Request, type Response} from "express";
 import cors from "cors";
 import helmet from "helmet";
+import authRouter from "./routes/auth.js"
+import { pinoHttp } from "pino-http";
+import logger from "./services/logger.js"
 
 const app: Express = express();
 app.use(cors({
@@ -10,12 +13,13 @@ app.use(cors({
 
 app.use(express.json());
 app.use(helmet());
+app.use(pinoHttp({logger}))
 
 app.get("/health", (req: Request, res: Response) => {
     res.send("Working!");
 });
 
-
+app.use("/api/auth", authRouter);
 
 export default app;
 
