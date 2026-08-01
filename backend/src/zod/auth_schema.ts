7aa68a56b30c51ae58708dbea2f07762f8c389fe
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-01 19:19:39 +0500

    Add: Zod validation schemas

--- backend/src/zod/auth_schema.ts
+++ backend/src/zod/auth_schema.ts
@@ -6,8 +6,23 @@ each auth schema must adhere to
 - ensure a min password length of 8
 - clamp password to a min and max as well */
 
-
-
-
 /*The same min and max clamping is applied for login schemas as well.
 These are the only known inputs from the frontend for the time being */
+
+export const register_schema = z.object({
+    body: z.object({
+        username: z.string().min(3).max(100),
+        email: z.string().min(3).max(100),
+        password: z.string().min(8).max(100)
+    })
+});
+
+export const login_schema = z.object({
+    body: z.object({
+        identifier: z.string().min(3).max(100),
+        password: z.string().min(8).max(100)
+    })
+});
+
+export type RegisterInput = z.infer<typeof register_schema>["body"];
+export type LoginInput = z.infer<typeof login_schema>["body"];
