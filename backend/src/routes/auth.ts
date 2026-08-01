4cf15b68f3951587e311becfed58ea057cc75036
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-01 22:38:46 +0500

    Hook zod validate with auth routes

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,13 +1,15 @@
 // Route containing authentication routes
 import { Router } from "express";
 import {register, login, logout} from "../controllers/authControllers.js"
+import { validate } from "../middleware/zod_middleware.js";
+import { register_schema, login_schema } from "../zod/auth_schema.js";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", validate(register_schema), register);
 
 
-router.post("/login",login);
+router.post("/login", validate(login_schema), login);
 
 router.post("/logout",logout);
 
