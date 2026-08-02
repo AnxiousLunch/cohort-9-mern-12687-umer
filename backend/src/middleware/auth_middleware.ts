ad7effb04f718102bb25aa058d7560fbc30e4c0a
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-02 18:56:34 +0500

    Add logic for auth middleware

--- backend/src/middleware/auth_middleware.ts
+++ backend/src/middleware/auth_middleware.ts
@@ -1,7 +1,27 @@
 // Beginning point for auth middleware
-
 // used as a sort of interceptor
 
 // for each request verify the cookie
 // valid -> pass
 // expired -> deny
+
+
+import type { NextFunction, Request, Response } from "express";
+import logger from "../services/logger.js";
+
+export default async function authenticate(req: Request, res: Response, next: NextFunction) {
+    try {
+        const auth = req.headers.authorization;
+        const token = auth?.startsWith("Bearer ") ? auth.split(" ")[1] : undefined;
+        
+        if (!token) {
+            logger.error("No token found");
+            return;
+        }
+        
+        // verify token logic here
+
+    } catch(err) {
+        next(err);
+    }
+}
