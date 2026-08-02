04ba032f8261a39d12cc36786d5397f99c6c345a
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-02 18:51:04 +0500

    init middlewares for auth and error handling

---
+++ backend/src/middleware/auth_middleware.ts
@@ -0,0 +1,7 @@
+// Beginning point for auth middleware
+
+// used as a sort of interceptor
+
+// for each request verify the cookie
+// valid -> pass
+// expired -> deny
---
+++ backend/src/middleware/error_middleware.ts
@@ -0,0 +1,6 @@
+// Beginning point for global error handling
+
+
+// Hook up with logger maybe
+
+// Replace every current instance with this
