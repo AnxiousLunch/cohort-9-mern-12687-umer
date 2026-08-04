6615b0230b37736ab6f3dad0585ed0dbf4b1a426
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-04 15:39:38 +0500

    fix types for AccessTokenPayload and Request.user

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -5,7 +5,7 @@ declare global {
     namespace Express {
         interface Request {
             user?: {
-                userId: string;
+                userId: Number;
                 username: string;
             };
         }
@@ -13,6 +13,6 @@ declare global {
 }
 
 export interface AccessTokenPayload extends JwtPayload {
-    userId: string;
+    userId: Number;
     username: string;
 }
