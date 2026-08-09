916011206489be9bc227eb3b610d545564d3541a
Umer <safee4600033@cloud.neduet.edu.pk>
2026-08-09 21:09:35 +0500

    Add protected route. Setup dashboard

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from "react"
+
+
+function Dashboard(): ReactElement {
+  return (
+    <>
+      <h1 className="text-2xl">Hello</h1>
+    </>
+  )
+}
+
+export default Dashboard;
