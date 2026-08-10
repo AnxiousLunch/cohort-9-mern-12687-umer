5236d071b6247f14264f9d35962a1bc6256c515c
Umer <safee4600033@cloud.neduet.edu.pk>
2026-08-10 20:39:40 +0500

    sidebar styling

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -58,16 +58,19 @@ function Dashboard(): ReactElement {
 
   return (
     <div className="flex h-screen bg-gray-50 text-gray-900"> 
+
+
       <div className="flex  flex-col bg-white w-50 border-r border-gray-200 p-10">
-        <div>
-          <button onClick={handleCreate}>
+
+        <div className="flex flex-col items-center px-4 py-6 justify-between border-b border-gray-200">
+          <h1 className="text-lg font-semibold">Notes</h1>
+          <button onClick={handleCreate}
+          className="rounded-md border border-gray-300 bg-white p-4 text-black hover:bg-gray-50">
             Create Note
           </button>
-          <button onClick={handleLogout}>
-            Logout
-          </button>
         </div>
-        <div>
+
+        <div className="flex-1 overflow-y-auto p-3 space-y-5">
           {notes.map((note) => (
             <button
               key={note.id}
@@ -76,6 +79,13 @@ function Dashboard(): ReactElement {
               {note.title || "Untitled"}
             </button>
           ))}
+        </div>
+
+        <div className="flex items-center p-4 justify-between border-b border-gray-200">
+          <button onClick={handleLogout}
+          className="rounder border border-gray-300 bg-white p-4 text-black hover:bg-gray-50">
+            Logout
+          </button>
         </div>
       </div>
 
