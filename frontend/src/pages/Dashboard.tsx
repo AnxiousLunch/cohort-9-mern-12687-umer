155ddd11a5b37b2f446e0c9652b28239b8d0d6b7
Umer <safee4600033@cloud.neduet.edu.pk>
2026-08-11 12:30:12 +0500

    make sidebar collapsable. Fix issue of sidebar overtaking content on note creation

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -149,36 +149,44 @@ function Dashboard(): ReactElement {
 
       {/* {sidebar continer} */}
       <div
-        className={`flex flex-col bg-white w-50 border-r border-gray-200 h-full transition-all duration-200 ${isSidebarCollapsed ? "w-16" : "w-64"}`}
+        className={`flex h-full flex-col shrink-0 border-b border-gray-200 ${isSidebarCollapsed ? "w-16" : "w-64"}`}
       >
         {/* sidebar header */}
-        <div className="flex flex-col items-center px-4 py-6 justify-between border-b border-gray-200">
-          <img
-            src={"src/assets/logo.png"}
-            alt="Notes logo"
-            className="h-10 w-10 shrink-0 object-contain"
-          />
+        <div className={`flex items-center ${isSidebarCollapsed ? "justify-center px-4 py-4" : "justify-between px-2 py-4"}`}>
 
-          {!isSidebarCollapsed && (
-            <div>
-              <h1 className="text-lg font-semibold">Notes</h1>
-              <button onClick={() => setIsSidebarCollapsed(true)}>←</button>
-            </div>
-          )}
-          {isSidebarCollapsed && (
-            <div>
-              <h1 className="text-lg font-semibold">Notes</h1>
-              <button onClick={() => setIsSidebarCollapsed(false)}>→</button>
-            </div>
-          )}
+          <div className="flex items-center gap-3">
+
+            <img
+              src={"src/assets/logo.png"}
+              alt="Notes logo"
+              className="h-10 w-10 shrink-0 object-contain"
+            />
+
+            {!isSidebarCollapsed && (
+              <h1 className="text-lg font-semibold">
+                Notes
+              </h1>
+            )}
+          </div>
+
+
           <button
-            onClick={handleCreate}
-            className="rounded-md border border-gray-300 bg-white p-4 text-black hover:bg-gray-50"
-          >
-            Create Note
+          onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+          className="rounded-md px-2 py-1 text-gray-500 hover:bg-gray-100">
+            {isSidebarCollapsed ? "-->" : "<--"}
           </button>
         </div>
 
+        <div className="px-3 pb-3">
+          <button
+            onClick={handleCreate}
+            className={`rounded-md border border-gray-300 bg-white p-4 text-black hover:bg-gray-50 w-full
+            ${isSidebarCollapsed ? "text-xl" : "px-4;"}`}
+          >
+            {isSidebarCollapsed ? "+" : "Create Note"}
+          </button>
+        </div>
+    
         <div className="flex-1 overflow-y-auto p-3 space-y-1">
           {notes.map((note) => (
             <button
