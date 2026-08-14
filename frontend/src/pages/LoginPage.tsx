678a3ad90acf82aaded6a96bc0eb6b4b125a015c
Umer <safee4600033@cloud.neduet.edu.pk>
2026-08-14 12:46:44 +0500

    styling: login page - add components, text

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,98 +3,106 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import axios from "axios";
 
-// export default function LoginPage(): ReactElement {
-//   const { login, isLoading } = useAuth();
-//   const navigate = useNavigate();
-//   const location = useLocation();
+export default function LoginPage(): ReactElement {
+  const { login, isLoading } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-//   const [identifier, setIdentifier] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState<string | null>(null);
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
-//   const from =
-//     (location.state as { from?: string } | null)?.from ?? "/dashboard";
+  const from =
+    (location.state as { from?: string } | null)?.from ?? "/dashboard";
 
-//   const onSubmit = async (e: FormEvent) => {
-//     e.preventDefault();
-//     setError(null);
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setError(null);
 
-//     try {
-//       await login(identifier, password);
-//       navigate(from, { replace: true });
-//     } 
-//     catch (err: any) {
-//       if (axios.isAxiosError(err)) {
-//         setError(err.response?.data?.msg || "Login failed");
-//       } else if (err instanceof Error) {
-//         setError(err.message);
-//       } else {
-//         setError("Something went wrong");
-//       }
-//     };
-//   }
+    try {
+      await login(identifier, password);
+      navigate(from, { replace: true });
+    } 
+    catch (err: any) {
+      if (axios.isAxiosError(err)) {
+        setError(err.response?.data?.msg || "Login failed");
+      } else if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Something went wrong");
+      }
+    };
+  }
 
-//   return (
-//     <main className="min-h-screen flex items-center justify-center p-6">
-//       <div className="w-full max-w-sm">
-//         <h1 className="text-2xl font-semibold mb-6">Log in</h1>
-
-//         <form onSubmit={onSubmit} className="space-y-4">
-//           <input
-//             type="text"
-//             autoComplete="username"
-//             required
-//             value={identifier}
-//             onChange={(e) => setIdentifier(e.target.value)}
-//             placeholder="Username or email"
-//             className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-gray-500"
-//           />
-
-//           <input
-//             type="password"
-//             autoComplete="current-password"
-//             required
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//             placeholder="Password"
-//             className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-gray-500"
-//           />
-
-//           {error && (
-//             <p className="text-sm text-red-600">
-//               {error}
-//             </p>
-//           )}
-
-//           <button
-//             type="submit"
-//             disabled={isLoading}
-//             className="w-full rounded bg-gray-900 px-3 py-2 text-white hover:bg-gray-800 disabled:opacity-50"
-//           >
-//             {isLoading ? "Logging in…" : "Log in"}
-//           </button>
-//         </form>
-
-//         <p className="mt-6 text-sm text-gray-600">
-//           Don't have an account?{" "}
-//           <Link
-//             to="/signup"
-//             className="text-gray-900 font-medium hover:underline"
-//           >
-//             Sign up
-//           </Link>
-//         </p>
-//       </div>
-//     </main>
-//   );
-// }
-
-
-export default function LoginScreen() {
   return (
-      <div className="bg-white w-64 h-64 rounded-xl">
+    <main className="min-h-screen flex items-center justify-center p-6">
+      <div className="flex flex-col">
+        <div>
+          <h1>Welcome Back</h1>
+          <div className="border-1 ">Keep thoughts that matter</div>
+        </div>
+        <div className="w-full max-w-sm">
+
+          <form onSubmit={onSubmit} className="space-y-4">
+            <input
+              type="text"
+              autoComplete="username"
+              required
+              value={identifier}
+              onChange={(e) => setIdentifier(e.target.value)}
+              placeholder="Username or email"
+              className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-gray-500"
+            />
+
+            <input
+              type="password"
+              autoComplete="current-password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:border-gray-500"
+            />
+
+            {error && (
+              <p className="text-sm text-red-600">
+                {error}
+              </p>
+            )}
+
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="w-full rounded bg-gray-900 px-3 py-2 text-white hover:bg-gray-800 disabled:opacity-50"
+            >
+              {isLoading ? "Logging in…" : "Log in"}
+            </button>
+          </form>
+
+          <p className="mt-6 text-sm text-gray-600">
+            Don't have an account?{" "}
+            <Link
+              to="/signup"
+              className="text-gray-900 font-medium hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <NotePreviewComponent/>
+      </div>
+    </main>
+  );
+}
+
+
+export function NotePreviewComponent() {
+  return (
+      <div className="bg-white w-80 h-80 rounded-xl border-1">
         
-        <div className="bg-red-600 w-64 h-8 rounded-t-xl top-0 flex items-center px-1 py-2 justify-between">
+        <div className="bg-red-600 w-full h-8 rounded-t-xl top-0 flex items-center px-1 py-2 justify-between">
           <div className="">Untitled</div>
           <div className="flex gap-3">
             <div className="w-4 h-4 bg-green-600 rounded-xl"></div>
@@ -104,7 +112,7 @@ export default function LoginScreen() {
         </div>
 
 
-        <div>
+        <div className="mt-4 px-4">
           <h1>Things to remember:</h1>
           <ul>
             <li>Buy eggs</li>
