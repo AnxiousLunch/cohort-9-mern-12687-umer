306ba3ecd718994ea69c8fc5b739508dd0370dd9
AnxiousLunch <muhammadumersafee@gmail.com>
2026-08-20 22:10:51 +0500

    - make sidebar flush - fix spacing - add cta when no notes created

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -23,14 +23,12 @@ function Dashboard(): ReactElement {
   const [content, setContent] = useState("");
   const [error, setError] = useState<string | null>(null);
 
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
 
   const [saveStatus, setSaveStatus] = useState<"saved" | "saving" | "error">("saved");
 
   const saveTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const skipAutoSave = useRef(false);
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-
   const lastUpdateRef = useRef<string | null>(null);
 
   const editor = useEditor({
@@ -177,41 +175,6 @@ function Dashboard(): ReactElement {
     };
   }, [title, content]);
 
-  const applyFormatting = (prefix: string, suffix: string) => {
-    const textarea = textareaRef.current;
-    if (!textarea) {
-      return;
-    }
-    const start = textarea.selectionStart;
-    const end = textarea.selectionEnd;
-
-    const selectedArea = content.substring(start, end);
-
-    const updatedContent = content.substring(0, start) + prefix + selectedArea + suffix + content.substring(end);
-
-    setContent(updatedContent);
-
-  }
-
-  const formatEntireLine = (prefix: string) => {
-    const textarea = textareaRef.current;
-    if (!textarea) {
-      return;
-    }  
-    const start = textarea.selectionStart;
-    const end = textarea.selectionEnd;
-
-    const selectedArea = content.substring(start, end);
-    
-    const lines = selectedArea.split('\n');
-    const newlines = lines.map(line => line.trim() ? prefix + line : line);
-    const newselectedArea = newlines.join('\n');
-
-    const newcontent = content.substring(0, start) + newselectedArea + content.substring(end);
-
-  }
-
-
   // useEffect(() => {
   //   if (selectedNote) {
   //     setTitle(selectedNote.title || "");
@@ -251,7 +214,7 @@ function Dashboard(): ReactElement {
 
   return (
     // Main container
-    <div className="flex flex-col h-screen p-3 gap-3 font-mono bg-[#282828]">
+    <div className="flex flex-col h-screen font-mono bg-[#282828]">
       {error && (
         <div className="absolute top-5 right-5 z-100 rounded-md border border-[#fb4934] bg-[#fb4934] px-4 py-4 text-sm text-[#282828]">
           {error}
@@ -259,7 +222,16 @@ function Dashboard(): ReactElement {
       )}
 
       {/* tooldbar */}
-      <div className="flex flex-row bg-[#3c3836] h-10 shrink-0 items-center gap-2 px-3 rounded-lg ">
+      <div className="relative z-50 flex flex-row h-12 shrink-0 bg-[#3c3836] h-10 shrink-0 items-center gap-2 px-3 rounded-lg mb-3 ">
+        
+        <button className="rounded-md px-2 py-2 text-xl text-[#ebdbb2] hover:text-[#504945]"
+        
+        onClick={() => {
+          setIsSidebarCollapsed(!isSidebarCollapsed);
+        }}>
+          ☰
+        </button>
+        
         <div className="flex items-center gap-2">
           <button className="rounded-md px-2 py-1 text-sm text-[#ebdbb2] hover:bg-[#504945] transition"
           onClick={() => editor?.chain().focus().toggleBold().run()}>B</button>
@@ -285,76 +257,78 @@ function Dashboard(): ReactElement {
       </div>
       
       {/* {sidebar continer} */}
-      <div className="flex flex-1 min-h-0 gap-3">
+      <div className="relative flex flex-1 min-h-0 gap-3">
+        {!isSidebarCollapsed && (
 
-        <div
-          className={`flex h-full flex-col rounded-2xl border shrink-0 border-b border-[#504945] bg-[#3c3836] ${isSidebarCollapsed ? "w-16" : "w-64"}`}
-        >
-          {/* sidebar header */}
-          <div className={`flex items-center ${isSidebarCollapsed ? "justify-center px-4 py-4" : "justify-between px-2 py-4"}`}>
+          <div
+            className={`flex h-full flex-col rounded-2xl border shrink-0 border-b border-[#504945] bg-[#3c3836] ${isSidebarCollapsed ? "w-16" : "w-64"}`}
+          >
+            {/* sidebar header */}
+            <div className={`flex items-center ${isSidebarCollapsed ? "justify-center px-4 py-4" : "justify-between px-2 py-4"}`}>
 
-            <div className="flex items-center gap-3">
+              <div className="flex items-center gap-3">
 
-              <img
-                src={logo}
-                alt="Notes logo"
-                className="h-10 w-10 shrink-0 object-contain"
-              />
+                <img
+                  src={logo}
+                  alt="Notes logo"
+                  className="h-10 w-10 shrink-0 object-contain"
+                />
 
-              {!isSidebarCollapsed && (
-                <h1 className="text-lg font-semibold text-[#ebdbb2]">
-                  Notes
-                </h1>
-              )}
+                {!isSidebarCollapsed && (
+                  <h1 className="text-lg font-semibold text-[#ebdbb2]">
+                    Notes
+                  </h1>
+                )}
+              </div>
+
+
+              <button
+              onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+              className="rounded-md px-2 py-1 text-[#928374] hover:bg-[#504945]">
+                {isSidebarCollapsed ? "→" : "←"}
+              </button>
             </div>
 
-
-            <button
-            onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
-            className="rounded-md px-2 py-1 text-[#928374] hover:bg-[#504945]">
-              {isSidebarCollapsed ? "→" : "←"}
-            </button>
-          </div>
-
-          <div className="px-3 pb-3">
-            <button
-              onClick={handleCreate}
-              className={`rounded-md border border-[#504945] bg-[#282828] p-4 text-[#ebdbb2] hover:bg-[#504945] w-full
-              ${isSidebarCollapsed ? "text-xl" : "px-4;"}`}
-            >
-              {isSidebarCollapsed ? "+" : "Create Note"}
-            </button>
-          </div>
-      
-          <div className="flex-1 overflow-y-auto p-3 space-y-1">
-            {notes.map((note) => (
+            <div className="px-3 pb-3">
               <button
-                key={note.id}
-                onClick={() => {
-                  setSelectedId(note.id)
-                  const mynote = notes.find((n) => n.id == note.id);
-                  if (mynote) {
-                    lastUpdateRef.current = mynote.updatedAt;
-                  }
-                }}
-                className={`px-2 py-2 w-full rounded-md text-sm transition ${note.id == selectedId ? "bg-[#675e59] text-[#ebdbb2]" : "text-[#ebdbb2] hover:bg-[#50942]"}`}
+                onClick={handleCreate}
+                className={`rounded-md border border-[#504945] bg-[#282828] p-4 text-[#ebdbb2] hover:bg-[#504945] w-full
+                ${isSidebarCollapsed ? "text-xl" : "px-4;"}`}
               >
-                {note.title || "Untitled"}
+                {isSidebarCollapsed ? "+" : "Create Note"}
               </button>
-            ))}
-          </div>
+            </div>
+        
+            <div className="flex-1 overflow-y-auto p-3 space-y-1">
+              {notes.map((note) => (
+                <button
+                  key={note.id}
+                  onClick={() => {
+                    setSelectedId(note.id)
+                    const mynote = notes.find((n) => n.id == note.id);
+                    if (mynote) {
+                      lastUpdateRef.current = mynote.updatedAt;
+                    }
+                  }}
+                  className={`px-2 py-2 w-full rounded-md text-sm transition ${note.id == selectedId ? "bg-[#675e59] text-[#ebdbb2]" : "text-[#ebdbb2] hover:bg-[#50942]"}`}
+                >
+                  {note.title || "Untitled"}
+                </button>
+              ))}
+            </div>
 
-          <div className="flex items-center p-4 justify-between border-t border-[#504945]">
-            <button
-              onClick={handleLogout}
-              className="rounder border border-[#504945] bg-[#282828] p-4 text-[#ebdbb2] hover:bg-[#504945]"
-            >
-              Logout
-            </button>
+            <div className="flex items-center p-4 justify-between border-t border-[#504945]">
+              <button
+                onClick={handleLogout}
+                className="rounded-lg border border-[#504945] bg-[#282828] p-4 text-[#ebdbb2] hover:bg-[#504945]"
+              >
+                Logout
+              </button>
+            </div>
           </div>
-        </div>
+        )}
 
-        <div className="min-w-0 flex-1 p-4 overflow-y-auto border-[#504945] bg-[#3c3836] rounded-2xl">
+        <div className="relative z-0 min-w-0 flex-1 overflow-y-auto bg-[#3c3836] p-4">
           {selectedNote ? (
             <div className="mx-auto max-w-4xl p-8 h-full">
               <div className="px-6 py-4">
@@ -409,7 +383,17 @@ function Dashboard(): ReactElement {
               </div>
             </div>
           ) : (
-            <div className="text-[#928374]  flex h-full justify-center"> Nothing Selected </div>
+            <div className=" flex flex-col text-[#928374] items-center h-full justify-center"> 
+              <p>Nothing Selected</p>
+              <button
+                onClick={handleCreate}
+                className={`rounded-md border border-[#504945] bg-[#282828] text-[#ebdbb2] hover:bg-[#504945] transition  font-medium px-4 py-4 mt-4 text-sm`}
+              >
+                Create Note
+              </button> 
+            </div>
+            
+            
           )}
         </div>
       </div>
